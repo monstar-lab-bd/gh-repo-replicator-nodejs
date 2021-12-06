e97b66901f92e73cbab59ea224e23eab87ea4e89
Tanveer Hassan <tanveer.hassan@monstar-lab.com>
2021-12-06 13:36:40 +0600

    style: add linters and vscode recommendations

--- src/index.ts
+++ src/index.ts
@@ -3,31 +3,30 @@
 import * as inquirer from 'inquirer';
 import * as yargs from 'yargs';
 
-
 const QUESTIONS = [
-    {
-        name: 'Source Url',
-        message: 'Enter the source repository github url :',
-        type: 'input',
-        when: () => !yargs.argv['source_url'],
-        validate: (input: string) => {
-            if (/(?:git|ssh|https?|git@[-\w.]+):(\/\/)?(.*?)(\.git)(\/?|\#[-\d\w._]+?)$/.test(input)) {
-              //@todo: download the repo and save as a zip file.
-              return true;
-            }
-            else {
-              return 'Not a valid git repository url.';
-            }
-        }
-    }
+  {
+    name: 'Source Url',
+    message: 'Enter the source repository github url :',
+    type: 'input',
+    when: () => !yargs.argv.source_url,
+    validate: (input: string) => {
+      if (
+        /(?:git|ssh|https?|git@[-\w.]+):(\/\/)?(.*?)(\.git)(\/?|\#[-\d\w._]+?)$/.test(
+          input,
+        )
+      ) {
+        // @todo: download the repo and save as a zip file.
+        return true;
+      }
+      return 'Not a valid git repository url.';
+    },
+  },
 ];
 
-inquirer.prompt(QUESTIONS)
-    .then(answers => {
+inquirer.prompt(QUESTIONS).then((answers: any) => {
+  const answer: any = { ...answers, ...yargs.argv };
 
-        let answer: any = Object.assign({}, answers, yargs.argv);
+  const gitSourceUrl = answer.source_url;
 
-        const gitSourceUrl = answer['source_url'];
-
-        console.log(gitSourceUrl)
-    });
+  console.log(gitSourceUrl);
+});
